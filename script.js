5582ee284e62da4914a8e731f0feb5a2dc544d24
Alexandre Carvalho <alexandrervdec@gmail.com>
2025-08-17 21:51:09 -0300

    Ajuste visual

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const nextBtn = document.querySelector(".carousel-btn.next");
 
 let cardWidth = carousel.querySelector("a").offsetWidth + 30;
 
+// Função que mostra/esconde botões
 function toggleButtons() {
   prevBtn.style.display = carousel.scrollLeft > 0 ? "block" : "none";
   nextBtn.style.display =
@@ -26,4 +27,20 @@ window.addEventListener("load", toggleButtons);
 window.addEventListener("resize", () => {
   cardWidth = carousel.querySelector("a").offsetWidth + 30;
   toggleButtons();
+});
+
+/* ===== ANIMAÇÃO DE ENTRADA ===== */
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("visible");
+      }
+    });
+  },
+  { threshold: 0.2 }
+);
+
+document.querySelectorAll(".certificados-carousel a").forEach((card) => {
+  observer.observe(card);
 });
